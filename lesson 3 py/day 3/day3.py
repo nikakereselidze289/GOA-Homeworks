211fc0adc73a985ff498b2ca7cc206a1a9035507
nikakereselidze289 <kereselidzenikoloz038@gmail.com>
2024-10-29 22:23:47 +0400

    commit

---
+++ lesson 3 py/day 3/day3.py
@@ -0,0 +1,24 @@
+# Defining the variables
+name = "Alice"
+age = 30
+city = "New York"
+is_student = False
+height = 5.6
+
+# Printing the values of each variable
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Is Student:", is_student)
+print("Height:", height)
+You sent
+# Defining the variables
+number1 = 15
+number2 = 10
+
+# Adding the variables
+
+# Printing the values and the result
+print("Number 1:", number1)
+print("Number 2:", number2)
+print("Sum:", sum_
