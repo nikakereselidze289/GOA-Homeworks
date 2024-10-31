2dd997d93a7dd2b7e26721858e0222c590d32799
nikakereselidze289 <kereselidzenikoloz038@gmail.com>
2024-10-31 18:11:30 +0400

    first commit

--- lesson 3 py/day 3/day3.py
+++ lesson 3 py/day 3/day3.py
@@ -11,14 +11,14 @@ print("Age:", age)
 print("City:", city)
 print("Is Student:", is_student)
 print("Height:", height)
-You sent
 # Defining the variables
 number1 = 15
 number2 = 10
 
 # Adding the variables
+sum = number1 + number2
 
 # Printing the values and the result
 print("Number 1:", number1)
 print("Number 2:", number2)
-print("Sum:", sum_
+print("Sum:", sum)
