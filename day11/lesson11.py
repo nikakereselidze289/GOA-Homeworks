0d9058a4f9f86232894bdc190fea233347edc2a1
nikakereselidze289 <kereselidzenikoloz038@gmail.com>
2024-11-19 19:45:50 +0400

    change name

---
+++ day11/lesson11.py
@@ -0,0 +1,18 @@
+print("და ოპერატორი")
+print(True and True)  #true
+print(True and False) #false
+print(False and True) #false
+print(False and False) #false
+
+
+first_number = int(input("first number: "))
+second_number = int(input("second number: "))
+
+print("first_number > 30 and second_number < 40") 
+
+
+print("ან ოპერატორი")
+print(True or True)  #true
+print(True or False) #true
+print(False or True) #true
+print(False or False) #false
