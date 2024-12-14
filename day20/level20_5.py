683b9d94a13219708285fce704e2f442a9e706d5
nikakereselidze289 <kereselidzenikoloz038@gmail.com>
2024-12-14 21:22:18 +0400

    correct mistakes

--- day20/level20_5.py
+++ day20/level20_5.py
@@ -2,13 +2,12 @@ num = int(input("Enter a number: "))
 
 if num <= 1:
     print(num, "is not a prime number.")
+elif num == 2:
+    print(num, "is a prime number.")  
 else:
-    is_prime = True
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            is_prime = False
+            print(num, "is not a prime number.")
             break
-    if is_prime:
-        print(num, "is a prime number.")
     else:
-        print(num, "is not a prime number.")
+        print(num, "is a prime number.")
