85c7db3d5bde244da8c33a0053a0592c7f063dfa
nikakereselidze289 <kereselidzenik823@gmail.com>
2025-02-06 16:18:34 +0400

    homeworks

--- day34/level34_9.py
+++ day34/level34_9.py
@@ -1,11 +1,5 @@
 def filter_positive_numbers(numbers):
-    positive_numbers = []
-    
-    for number in numbers:
-        if number > 0:
-            positive_numbers.append(number)
-    
-    return positive_numbers
+    return [number for number in numbers if number > 0]
 
 num_list = [-10, 15, -3, 7, 0, -1, 5, -6]
 positive_list = filter_positive_numbers(num_list)
