ba659c515ce51478b18e755ad4f409bcb95d1d68
nikakereselidze289 <kereselidzenik823@gmail.com>
2025-05-13 22:45:54 +0400

    GOA BEST

---
+++ day64/classwork/level64_1.js
@@ -0,0 +1,5 @@
+function concStrings() {
+  let firstString = prompt("შეიყვანეთ პირველი სტრინგი:");
+  let secondString = prompt("შეიყვანეთ მეორე სტრინგი:");
+  alert(firstString + secondString);
+}
